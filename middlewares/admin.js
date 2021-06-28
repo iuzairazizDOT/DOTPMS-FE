4d3a372a57181f93ed51cdc02c9f2fb9fe8de3dc
syedsaroshfarrukhdot <81307212+syedsaroshfarrukhdot@users.noreply.github.com>
2021-06-28 17:32:45 +0500

    Changes

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,7 +1,10 @@
-async function admin(req,res,next){
-
-    if(req.user.role != "Admin") return res.status(403).send("User Not Authorized");
-    next()
-
+async function admin(req, res, next) {
+  if (
+    req.user.role != "Admin" ||
+    req.user.role != "Project Manager" ||
+    req.user.role != "CEO"
+  )
+    return res.status(403).send("User Not Authorized");
+  next();
 }
 module.exports = admin;
