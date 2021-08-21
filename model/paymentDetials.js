cb98f4e4ce8cc53cc7649bbc2568266bb35489cb
syedsaroshfarrukhdot <81307212+syedsaroshfarrukhdot@users.noreply.github.com>
2021-08-21 19:44:17 +0500

    Changes

--- model/paymentDetials.js
+++ model/paymentDetials.js
@@ -2,18 +2,22 @@ const mongoose = require("mongoose");
 
 const paymentDetialsSchema = mongoose.Schema(
   {
-    projectPayments: {
+    project: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "ProjectPayment",
+      ref: "Project",
     },
-    recievedAmount: Number,
-    exchangeRate: Number,
-    PaymentRecievedDate: Date,
-    PaymentDescription: String,
+    paymentDetials: [
+      {
+        recievedAmount: Number,
+        exchangeRate: Number,
+        PaymentRecievedDate: Date,
+        PaymentDescription: String,
+      },
+    ],
   },
   { timestamps: true }
 );
 
-const paymentDetials = mongoose.model("paymentDetials", paymentDetialsSchema);
+const PaymentDetial = mongoose.model("PaymentDetial", paymentDetialsSchema);
 
-module.exports.paymentDetials = paymentDetials;
+module.exports.PaymentDetial = PaymentDetial;
